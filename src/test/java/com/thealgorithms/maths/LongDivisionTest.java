db6847d030289fc6daf19dd26f0d5341b7fa8078
OudayAhmed <66073097+OudayAhmed@users.noreply.github.com>
2023-02-19 15:01:35 +0100

    Fix #8: Fixed the errors in the LongDivisionTest.

--- src/test/java/com/thealgorithms/maths/LongDivisionTest.java
+++ src/test/java/com/thealgorithms/maths/LongDivisionTest.java
@@ -1,40 +1,31 @@
 package com.thealgorithms.maths;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LongDivisionTest {
 
-    @Test
-    void checkCoverage() {
-      boolean[] coverageFlags = new boolean[19];
-      for(boolean flag : coverageFlags)
-        flag = false;
-
-      testOne(coverageFlags);
-      testTwo(coverageFlags);
-      testThree(coverageFlags);
-       
-      for(int i=0;i<coverageFlags.length;i++) {
-        System.out.println("Branch " + i + ": " + coverageFlags[i]);
-      }
+    @AfterAll
+    static void checkCoverage() {
+        boolean[] coverageDivideFlags = LongDivision.getCoverageDivideFlags();
     }
 	
     @Test
-    void testOne(boolean[] coverageFlags) {
-        assertEquals(3, LongDivision.divide(10,3,coverageFlags));
+    void testOne() {
+        assertEquals(3, LongDivision.divide(10,3));
     }
   
       @Test
-    void testTwo(boolean[] coverageFlags) {
-        assertEquals(-2, LongDivision.divide(7,-3,coverageFlags));
+    void testTwo() {
+        assertEquals(-2, LongDivision.divide(7,-3));
     }
   
   
       @Test
-    void testThree(boolean[] coverageFlags) {
-        assertEquals(10, LongDivision.divide(105,10,coverageFlags));
+    void testThree() {
+        assertEquals(10, LongDivision.divide(105,10));
     }
 	
 }
