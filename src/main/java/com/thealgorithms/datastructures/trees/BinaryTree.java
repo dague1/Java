e8a10d55722ef00c051d64544fb895f1794c0eb7
David Ljunggren <davidxljunggren@gmail.com>
2023-02-20 18:02:41 +0100

    Ref #11: remove() now has a CC of 4 down from around 15

--- src/main/java/com/thealgorithms/datastructures/trees/BinaryTree.java
+++ src/main/java/com/thealgorithms/datastructures/trees/BinaryTree.java
@@ -163,7 +163,7 @@ public class BinaryTree {
     public boolean removeNoHelper(Node temp) {
             if (temp == root) {
                 root = null;
-            } // This if/else assigns the new node to be either the left or right child of the parent
+            } // Assigns the new node to be either the left or right child of the parent
             else if (temp.parent.data < temp.data) {
                 temp.parent.right = null;
             } else {
