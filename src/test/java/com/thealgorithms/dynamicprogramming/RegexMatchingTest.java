263470ccd566c0e1d1d7fa74f41b5fc5bbd66878
unknown <arenbro@kth.se>
2023-02-21 20:03:19 +0100

    Ref #8: Removed tests that were added by us.

--- src/test/java/com/thealgorithms/dynamicprogramming/RegexMatchingTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/RegexMatchingTest.java
@@ -15,48 +15,10 @@ public class RegexMatchingTest {
     }
 
     @Test
-    void methodOneShouldReturnFalseWhenPatLengthNotZeroOrChar() {
-        String input1 = "";
-        String input2 = "!";
-        assertFalse(RegexMatching.regexRecursion(input1, input2));
-    }
-
-    @Test
-    void methodOneShouldReturnFalseWhenPatLengthIsZeroAndSrcLengthIsNotZero() {
-        String input1 = "src";
-        String input2 = "";
-        assertFalse(RegexMatching.regexRecursion(input1, input2));
-    }
-
-    @Test
-    void methodOneShouldReturnTrueWhenSrcLengthAndPatLengthEqualsZero() {
-        String input1 = "";
-        String input2 = "";
-        assertTrue(RegexMatching.regexRecursion(input1, input2));
-    }
-
-    @Test
-    void methodOneShouldReturnTrueWhenSrcLengthIsZeroAndPatCharContainsStar() {
-        String input1 = "";
-        String input2 = "*";
-        assertTrue(RegexMatching.regexRecursion(input1, input2));
-    }
-    
-    @Test
-    void methodTwoShouldReturnTrueWhenSrcAndPatLengthEqualsSvidxAndPvidx() {
-        String input1 = "src";
-        String input2 = "pat";
-        int svidx = 3;
-        int pvidx = 3;
-        assertTrue(RegexMatching.regexRecursion(input1, input2, svidx, pvidx));
-    }
-
-    @Test
-    void methodTwoShouldReturnFalseWhenSrcAndPatLengthNotEqualToSvidxAndPvidx() {
-        String input1 = "sr";
-        String input2 = "*";
-        int svidx = 1;
-        int pvidx = 1;
-        assertFalse(RegexMatching.regexRecursion(input1, input2, svidx, pvidx));
+    /*
+    * Dummy test in order to be able to run coverage tool when no real tests are present
+    * */
+    void dummyTest() {
+        return;
     }
 }
